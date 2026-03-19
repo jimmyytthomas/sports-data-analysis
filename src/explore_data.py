da953d5a16a5a0e7bb8b693ee3b2ab183697efc1
Jimmy Thomas <jimmythomas@Jimmys-MacBook-Pro-2.local>
2026-03-19 10:56:03 -0700

    seperated scores

--- src/explore_data.py
+++ src/explore_data.py
@@ -2,4 +2,9 @@ import pandas as pd
 prem_data = pd.read_csv("dataset/pl_24-25_matches_clean.csv")
 #print(prem_data.head())
 #print(prem_data.columns)
-print(prem_data[["home_team", "away_team", "score"]])
+#print(prem_data[["home_team", "away_team", "score"]])
+#en dash if needed –
+first_score = prem_data["score"].iloc[0]
+
+print(int(first_score.split("–")[0]))
+print(int(first_score.split("–")[1]))
