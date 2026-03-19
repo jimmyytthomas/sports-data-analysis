d095eb3315a315c87d22fb16304e792633ee31b0
Jimmy Thomas <jimmythomas@Jimmys-MacBook-Pro-2.local>
2026-03-19 10:30:02 -0700

    printed home,away and socre data

---
+++ src/explore_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+prem_data = pd.read_csv("dataset/pl_24-25_matches_clean.csv")
+#print(prem_data.head())
+#print(prem_data.columns)
+print(prem_data[["home_team", "away_team", "score"]])
