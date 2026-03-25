4902eb0bd0da96f8c3ad378d18f15b1a3a244804
Jimmy Thomas <jimmythomas@Jimmys-MacBook-Pro-2.local>
2026-03-25 14:27:12 -0700

    update

--- src/explore_data.py
+++ src/explore_data.py
@@ -6,5 +6,15 @@ prem_data = pd.read_csv("dataset/pl_24-25_matches_clean.csv")
 #en dash if needed –
 first_score = prem_data["score"].iloc[0]
 
-print(int(first_score.split("–")[0]))
-print(int(first_score.split("–")[1]))
+home_goals = int(first_score.split("–")[0])
+away_goals = int(first_score.split("–")[1])
+
+result = None
+if home_goals > away_goals:
+    result = "home_win"
+elif home_goals < away_goals:
+    result = "away_win"
+else:
+    result = "draw"
+
+print(result)
